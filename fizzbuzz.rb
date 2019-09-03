af82bced95f9005b215c8b06e59dd8ac0ea02913
Albert Viilik <albert.viilik@gmail.com>
2019-09-03 00:33:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(n)
-  result = ""
-  if n % 3 == 0
-    result += "Fizz"
-  end
-  if n % 5 == 0
-    result += "Buzz"
-  end
-  if result == ""
+  if n % 3 == n % 5
+    return "FizzBuzz"
+  elsif n % 3 == 0
+    return "Fizz"
+  elsif n % 5 == 0
+    return "Buzz"
+  else
     return nil
   end
 end
